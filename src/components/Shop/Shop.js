ab46cc561098052c21ad7cc3e0471b9904897dd5
SakibSaizeed <sakibsaizeed@gmail.com>
2022-08-17 23:36:13 +0600

    Cart Calculation added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,42 +1,38 @@
 import React, { useEffect, useState } from "react";
 import Product from "../Products/Product";
 import "./Shop.css";
+import ShoppingCart from "../Shopping-Cart/ShoppingCart";
 const Shop = () => {
   const [products, setProduct] = useState([]);
-  const [cart,setCart]=useState([]);
+  const [cart, setCart] = useState([]);
   useEffect(() => {
     fetch("./products.json")
       .then((res) => res.json())
       .then((data) => setProduct(data));
   }, []);
-  function addToCart(product){
-    console.log(product);
+  function addToCart(product) {
+    // console.log(product);
     //cart.push(product);
-    const newCart=[...cart,product];
+    const newCart = [...cart, product];
     setCart(newCart);
-    console.log(newCart[0].name);
-
+    // console.log(newCart[0].name);
   }
 
-  
   return (
     <div className="shop-container">
       <div className="product-container">
         {/* <h1>Products Container</h1>{" "} */}
         {products.map((product) => (
-          <Product 
-          product={product}
-          key={product.id}
-          addToCart={addToCart}></Product>
+          <Product
+            product={product}
+            key={product.id}
+            addToCart={addToCart}
+          ></Product>
         ))}
       </div>
 
-      <div className="order-container">
-        <h2>Order Summury</h2>
-        <h4>Selected Items:{cart.length}</h4>
-        <h5>
-          
-        </h5>
+      <div className="cart-container">
+        <ShoppingCart cart={cart}></ShoppingCart>
       </div>
     </div>
   );
---
+++ src/components/Shopping-Cart/ShoppingCart.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./ShoppingCart.css";
+const ShoppingCart = (props) => {
+  const { cart } = props;
+  let total=0;
+  let shipping=0;
+  let tax=0;
+  let grandTotal=0;
+  for (const selected of cart) {
+    //console.log(selected);
+     total=total+selected.price;
+      shipping+=selected.shipping;
+      tax=parseFloat((total*0.1).toFixed(2));
+      
+      grandTotal=total+shipping+tax;
+    
+  }
+  return (
+    <div className="calculation-area">
+      <h2>Order Summary</h2>
+      <p>Selected Items: {cart.length}</p>
+      <p>Total Price: ${total}</p>
+      <p>Shipping_charge: ${shipping}</p>
+      <p>Tax: ${tax}</p>
+      <h4>Grand Total: ${grandTotal.toFixed(2)}</h4> 
+    </div>
+  );
+};
+
+export default ShoppingCart;
